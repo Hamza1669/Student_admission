bf0e30a3163c6035f7433bdbbb35e6ba0bdf27f3
Hamza1669 <hamzasajjad222@gmail.com>
2024-06-05 16:28:38 +0200

    Remove problematic directory

---
+++ example-student-admission-form/student-admission-form/src/test/java/com/studentapplication/studentadmissionform/StudentAdmissionFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentapplication.studentadmissionform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAdmissionFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
